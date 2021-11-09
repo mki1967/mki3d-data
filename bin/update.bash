2fa013b34c2e28951bb45c7e70751c523c00a33a
Marcin Kik <mki1967@gmail.com>
2021-11-09 08:05:15 +0100

    new `bin/update.bash` and removing files `INDEX_MKI3D_8000.html`

--- bin/update.bash
+++ bin/update.bash
@@ -2,11 +2,5 @@ BIN_DIR=`dirname $0`
 BIN_DIR=`cd "$BIN_DIR";pwd`
 cd $BIN_DIR/../data/
 
-for d in $(ls); do
-    pushd $d;
-    $BIN_DIR/make-public-idx.bash
-    $BIN_DIR/make-8000-idx.bash
-    popd ;
-done 
+$BIN_DIR/make-data-tree-idx.bash
 
-$BIN_DIR/make-main-idx.bash data-index.html
